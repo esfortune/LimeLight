7f2913c2426c95ab2b05abe2eeab6a4e7091166f
esfortune <eric.fortune@gmail.com>
2024-12-17 11:35:46 -0500

    Cleanup on aisle one

--- bin/dockerDUFS.sh
+++ bin/dockerDUFS.sh
@@ -4,7 +4,6 @@
 # to come up and down.
 
 nmcli connection up LL2024v01rev01
+# Usually IP address: 10.42.0.1 
 
 docker run -v /home/arducam/data:/data -p 5000:5000 --rm sigoden/dufs /data -A
-
-# Usually IP address: 10.42.0.1 
