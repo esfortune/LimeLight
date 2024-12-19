f3bbbe31ea712e3f4313cac7caac6982897fdb14
esfortune <eric.fortune@gmail.com>
2024-12-19 18:52:04 -0500

    Added comment to dockerDUFS.sh

--- bin/dockerDUFS.sh
+++ bin/dockerDUFS.sh
@@ -7,3 +7,8 @@ nmcli connection up LL2024v01rev01
 # Usually IP address: 10.42.0.1 
 
 docker run -v /home/arducam/data:/data -p 5000:5000 --rm sigoden/dufs /data -A
+
+# curl -sSL https://get.docker.com | sh
+# sudo usermod -aG docker $USER
+
+
