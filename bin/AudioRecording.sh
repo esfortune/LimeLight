c60839917b18508c0d79dd2f7da6af06fcd1e05c
esfortune <eric.fortune@gmail.com>
2024-12-25 00:58:47 -0500

    Updated the crontab. Need to fix GPS so that it only does up to 10 seconds or so.

--- bin/AudioRecording.sh
+++ bin/AudioRecording.sh
@@ -3,6 +3,6 @@
 # Code written for Limelight Rainforest test device.
 # This is code for recording from the current microphone, which is
 # an Audiomoth flashed as a microphone, on a Raspberry Pi 5. 
-# 384000 Hz sample rate, 30 sec duration.
+# 384000 Hz sample rate, 60 sec duration.
 
-arecord -D hw:CARD=Microphone,DEV=0 -f S16_LE -r 384000 -d 30 /home/arducam/foobar.wav
+arecord -D hw:CARD=Microphone,DEV=0 -f S16_LE -r 384000 -d 60 /home/arducam/foobar.wav
