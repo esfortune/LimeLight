9889656a803fe86887242767b2a619e61cf8cfbd
esfortune <eric.fortune@gmail.com>
2025-06-16 01:52:12 -0400

    Added some scripts to backup

---
+++ clbin/clImages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "JPG files in curdat:" `find ~/data/curdat | grep -ci jpg`
---
+++ clbin/dailyBackup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright Eric Fortune, CanopyLife, June 2025
+# Code written for Mothpad device.
+
+#########################################################################
+## SET VARIABLES
+
+datestamp=$(date +"%Y%b%d")
+
+dataDIR=`grep data_path /home/canopylife/bin/config.py | sed -n "s/^.*'\(.*\)'.*$/\1/p"`
+
+# This is our backup path from config.py with the current date
+backupDIR=`grep backup_path /home/canopylife/bin/config.py | sed -n "s/^.*'\(.*\)'.*$/\1/p"`/$datestamp
+
+echo $dataDIR $backupDIR
+
+if [ -d "$dataDIR" ]; then
+   # Directory the data directory exists, we can move forward
+
+   if [ ! -d "$backupDIR" ]; then
+      # If backup directory doesn't exist, then make it
+      mkdir -p "$backupDIR"
+   fi
+
+   mv $dataDIR/* $backupDIR
+
+fi
+
