fe484bd1be24d2d82361cfaa91d535d3d3552ca6
esfortune <eric.fortune@gmail.com>
2025-07-20 13:44:52 -0500

    Much improved dailyBackup.sh

--- clbin/dailyBackup.sh
+++ clbin/dailyBackup.sh
@@ -5,27 +5,143 @@
 #########################################################################
 ## SET VARIABLES
 
-datestamp=$(date +"%Y%b%d")
-
 dataDIR=`grep data_path /home/canopylife/bin/config.py | sed -n "s/^.*'\(.*\)'.*$/\1/p"`
 
-# This is our backup path from config.py with the current date
-backupDIR=`grep backup_path /home/canopylife/bin/config.py | sed -n "s/^.*'\(.*\)'.*$/\1/p"`/$datestamp
+numPics=`clImages.sh | sed 's/[^0-9]*//g'`
 
-echo $dataDIR $backupDIR
+echo $dataDIR $numPics
 
-if [ -d "$dataDIR" ]; then
-   # Directory the data directory exists, we can move forward
+if [ "$numPics" -gt "1" ]; then
 
-   if [ ! -d "$backupDIR" ]; then
-      # If backup directory doesn't exist, then make it
-      mkdir -p "$backupDIR"
-   fi
+    # cd into the data directory
+    cd $dataDIR/Studio
 
-   mv $dataDIR/* $backupDIR
+    # Get the first filname
+    earliestFile=`ls -t *.jpg | tail -1`
+
+    # Convert to datestamp ########################################
+
+    # Extract the year and MMDD using parameter expansion and cut
+    year=$(echo "$earliestFile" | cut -d'_' -f3)
+    mmdd=$(echo "$earliestFile" | cut -d'_' -f4 | cut -c1-4)
+
+    # Extract month and day
+    month=${mmdd:0:2}
+    day=${mmdd:2:2}
+
+    case $month in
+      01) month_abbr="Jan" ;;
+      02) month_abbr="Feb" ;;
+      03) month_abbr="Mar" ;;
+      04) month_abbr="Apr" ;;
+      05) month_abbr="May" ;;
+      06) month_abbr="Jun" ;;
+      07) month_abbr="Jul" ;;
+      08) month_abbr="Aug" ;;
+      09) month_abbr="Sep" ;;
+      10) month_abbr="Oct" ;;
+      11) month_abbr="Nov" ;;
+      12) month_abbr="Dec" ;;
+      *)  month_abbr="Err" ;;
+    esac
+
+    # Compose datestamp
+    datestamp="${year}${month_abbr}${day}"
+
+echo $datestamp
+
+    # And now we have our datestamp ###############################
+
+    # This is our backup path from config.py with the current date
+    backupDIR=`grep backup_path /home/canopylife/bin/config.py | sed -n "s/^.*'\(.*\)'.*$/\1/p"`/$datestamp
+
+echo $backupDIR
+
+    # Create backup directory if it doesn't exist
+    mkdir -p "$backupDIR"
+
+    # Function to copy and rename file if name already exists
+    copy_with_suffix() {
+        src_file="$1"
+        dest_dir="$2"
+        filename=$(basename -- "$src_file")
+        base="${filename%.*}"
+        ext="${filename##*.}"
+
+        if [ "$ext" != "$filename" ]; then
+            newname="$base.$ext"
+        else
+            newname="$base"
+        fi
+
+        suffix="a"
+
+        while [ -e "$dest_dir/$newname" ]; do
+            if [ "$ext" != "$filename" ]; then
+                newname="${base}-${suffix}.$ext"
+            else
+                newname="${base}-${suffix}"
+            fi
+            suffix=$(echo "$suffix" | tr "0-9a-y" "1-9a-z")
+        done
+
+        # cp "$src_file" "$dest_dir/$newname" # COPY
+        mv "$src_file" "$dest_dir/$newname"   # MOVE
+    }
+
+    # Copy Studio directory
+    if [ -d "$dataDIR/Studio" ]; then
+        mkdir -p "$backupDIR/Studio"
+        for f in "$dataDIR/Studio/"*; do
+            [ -e "$f" ] || continue
+            copy_with_suffix "$f" "$backupDIR/Studio"
+        done
+    fi
+
+    # Copy Audio directory
+    if [ -d "$dataDIR/Audio" ]; then
+        mkdir -p "$backupDIR/Audio"
+        for f in "$dataDIR/Audio/"*; do
+            [ -e "$f" ] || continue
+            copy_with_suffix "$f" "$backupDIR/Audio"
+        done
+    fi
+
+    # Copy txt files
+    for f in "$dataDIR"/*.txt; do
+        [ -e "$f" ] || continue
+        copy_with_suffix "$f" "$backupDIR"
+    done
+
+    # Copy csv files
+    for f in "$dataDIR"/*.csv; do
+        [ -e "$f" ] || continue
+        copy_with_suffix "$f" "$backupDIR"
+    done
+
+################ Old mover
+#    if [ -d "$dataDIR" ]; then
+#       # Directory the data directory exists, we can move forward
+#
+#       if [ ! -d "$backupDIR" ]; then
+#          # If backup directory doesn't exist, then make it
+#          mkdir -p "$backupDIR"
+#       fi
+#
+#       # move the directories
+#       mv $dataDIR/Studio $backupDIR/
+#       mv $dataDIR/*.txt $backupDIR/
+#       mv $dataDIR/*.csv $backupDIR/
+#       if [ -d "$dataDIR/Audio" ]; then
+#            mv $dataDIR/Audio $backupDIR/
+#       fi
+#
+#    fi
+################
 
 fi
 
+######################################################################################
 ## Check to see the position of the switch. If it is 1, then we are in "standard" mode
 ## and we should make the timelapse movie.
 
@@ -35,6 +151,6 @@ status=$?
 if [ $status = 1 ]; then
 
    # Make the movie
-   /home/canopylife/bin/movieMaker.sh $backupDIR
+   movieMaker.sh $backupDIR $datestamp
 
 fi
--- clbin/movieMaker.sh
+++ clbin/movieMaker.sh
@@ -24,7 +24,21 @@ if [ -d "$workingDIR" ]; then
    cd smaller
 
    ffmpeg -framerate 30 -pattern_type glob -i '*.pnm' -c:v libx264 -pix_fmt yuv420p output.mp4
-   mv output.mp4 "../${serial}_${location}_${datestamp}.mp4"
+
+#   mv output.mp4 "../${serial}_${location}_${datestamp}.mp4"  ### Old MOVER
+
+    dest_base="../${serial}_${location}_${datestamp}"
+    dest_file="${dest_base}.mp4"
+
+    # Check if the file already exists, and if so, keep going until we have a new unique name
+    suffix=a
+    while [ -e "$dest_file" ]; do
+        dest_file="${dest_base}-${suffix}.mp4"
+        suffix=$(echo "$suffix" | tr "0-9a-y" "1-9a-z")
+    done
+
+    # Move with final unique name
+    mv output.mp4 "$dest_file"
 
    cd ..
    rm -rf smaller
