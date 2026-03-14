b860dd673262d650a16558f4be2e00aed5a5ff91
Eric Fortune <eric.fortune@gmail.com>
2026-03-14 18:25:42 -0400

    Fixed the new GPIOSET version (compatible with 2.2.1).  VERY FRUSTRATING!

--- clbin/attractorON.sh
+++ clbin/attractorON.sh
@@ -10,9 +10,13 @@ attractorON=`grep attractorON_gpio /home/canopylife/bin/config.py | sed 's/[^0-9
 # Pull up the GPIO pin to trigger the locking relay, then turn it off.
 # The pulse is 1 second in duration - we use the sleep command.
 
-gpioset gpiochip0 $attractorON=1         # Set GPIO high
-sleep 1
-gpioset gpiochip0 $attractorON=0         # Set GPIO to low
+## OLD METHOD up to 2025
+# gpioset gpiochip0 $attractorON=1         # Set GPIO high                                                                                                                       
+# sleep 1                                                                                                                                                                     
+# gpioset gpiochip0 $attractorON=0         # Set GPIO to low                                                                                                                     
+
+## NEW SYNTAX since 2026
+gpioset --chip gpiochip0 --toggle 200ms,0 $attractorON=1   # Pulse GPIO high for 200ms then low
 
 timestamp=$(date +"%Y%m%d%H%M%S")
 echo $timestamp", Attractor LEDs ON"
--- clbin/studioON.sh
+++ clbin/studioON.sh
@@ -10,9 +10,13 @@ studioON=`grep studioON_gpio /home/canopylife/bin/config.py | sed 's/[^0-9]//g'`
 # Pull up the GPIO pin to trigger the locking relay, then turn it off.
 # The pulse is 1 second in duration - we use the sleep command.
 
-gpioset gpiochip0 $studioON=1         # Set GPIO high
-sleep 1
-gpioset gpiochip0 $studioON=0         # Set GPIO to low
+## OLD METHOD UP TO 2025
+# gpioset gpiochip0 $studioON=1         # Set GPIO high                                                                                                                       
+# sleep 1                                                                                                                                                                     
+# gpioset gpiochip0 $studioON=0         # Set GPIO to low                                                                                                                     
+
+## NEW SYNTAX SINCE 2026
+gpioset --chip gpiochip0 --toggle 200ms,0 $studioON=1   # Pulse GPIO high for 200ms then low
 
 timestamp=$(date +"%Y%m%d%H%M%S")
 echo $timestamp", Studio LEDs ON"
