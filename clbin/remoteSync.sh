f13ab2eb48953316a6e03a2e019ccb473781bbc7
Eric Fortune <eric.fortune@njit.edu>
2026-03-20 15:39:37 -0600

    Updated timezone for wDeviceInfo.py. Added draft remoteSync.sh

---
+++ clbin/remoteSync.sh
@@ -0,0 +1,4 @@
+nohup rclone copy --no-traverse /home/arducam/data/Jotta/03-24-2025 CanopyLife:MothPadData/117608014/03242025 &
+
+rclone copy --max-age 24h --no-traverse /path/to/src remote:
+
--- clbin/wDeviceInfo.py
+++ clbin/wDeviceInfo.py
@@ -1,5 +1,5 @@
 #!/home/canopylife/PyEnvs/neopixel/bin/python3
-# Eric Fortune, Canopy Life, April 2025
+# Eric Fortune, Canopy Life, March 2026
 # Code written for MothPad device.
 # Update the system status for web user
 # 
@@ -10,7 +10,7 @@ import subprocess
 
 import config as c
 
-statusLED = c.statusLED
+# statusLED = c.statusLED # Hidden until I'm sure about what to do with the indicator LED
 
 deviceSerial = "SN: " + c.deviceSerial
 deviceLocation = "Location: " + c.deviceLocation
@@ -25,19 +25,22 @@ diskUsage = str(round(diskTotal / (1024**3))) + "Gb, " + str(round(diskUsed / (1
 
 diskPerCentRemaining = "Percent Free:  " + str(round((diskFree / diskTotal) * 100)) + " %"
 
+tzResult = subprocess.run(["cat", "/etc/timezone"], capture_output=True, text=True)
+timeZone = "Time Zone: " + tzResult.stdout.strip()
+
 currentMode = subprocess.run([c.checkMode], capture_output=True, text=True)
 if currentMode.returncode == 1:
     modeMSG = "Normal Mode"
 if currentMode.returncode == 0:
     modeMSG = "LowPower Mode"
 
-ledBlink = subprocess.Popen([statusLED, '2'])
+# ledBlink = subprocess.Popen([statusLED, '2'])
 
 print(f"{deviceSerial}, {deviceLocation}, {deviceGPS}")
-print(f"{printTime}")
+print(f"{printTime}, {timeZone}")
 print(f"{diskUsage}, {diskPerCentRemaining}")
 
-time.sleep(5)
+# time.sleep(5)
 
-ledBlink.kill()
-ledBlink = subprocess.Popen([statusLED, '6'])
+# ledBlink.kill()
+# ledBlink = subprocess.Popen([statusLED, '6'])
